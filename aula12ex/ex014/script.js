a4dbef5fafb5bb7c72edc4b619e19b31638172a1
Joao Guilherme <joaoguilhermeferreira19@gmail.com>
2023-02-28 23:59:04 -0300

    Comentários aula 12

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,23 +1,23 @@
 function carregar() {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var cump = document.getElementById('cump')
-    var data = new Date()
-    var hora = data.getHours()
-    //var hora = 2
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var msg = document.getElementById('msg') // var referente à div da mensagem dizendo as horas
+    var img = document.getElementById('imagem') // var referente à imagem que será "escolhida" dependendo da hora
+    var cump = document.getElementById('cump') // var referente à div da mensagem que cumprimenta o usuário
+    var data = new Date() //o operador new cria uma nova instância de objeto definido pelo usuário ou um tipo nativo, que é o caso dessa aplicação que usa Date.
+    var hora = data.getHours() //a var "hora" recebe o valor das horas do objeto Date contido na variável "data" através do método .getHours()
+    //var hora = 2 //o comentário ao lado foi usado para testar o programa sem necessitar de mudar as horas no cliente
+    msg.innerHTML = `Agora são ${hora} horas.` //edita o conteudo da div "msg"
 
-    if(hora >= 0 && hora< 12) 
+    if(hora >= 0 && hora< 12) //testa horário da manhã
     {
         img.src = 'fotomanha.png'
-        document.body.style.background = '#89587e'
-        cump.innerHTML = 'Bom dia!'
-    } else if(hora >= 12 && hora <= 18)
+        document.body.style.background = '#89587e' //muda cor do background
+        cump.innerHTML = 'Bom dia!' //edita conteúdo da div 'cump'
+    } else if(hora >= 12 && hora <= 18) //testa horário da tarde
     {
         img.src = 'fototarde.png'
         document.body.style.background = '#f7b35d'
         cump.innerHTML = 'Boa tarde!'
-    } else{
+    } else{ //logicamente, se não é manhã nem tarde, será noite
         img.src = 'fotonoite.png'
         document.body.style.background = '#31464c'
         cump.innerHTML = 'Boa noite!'
--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,19 +1,19 @@
 function verificar(){
-    var data = new Date();
+    var data = new Date(); //vide script ex014
     var ano = data.getFullYear();
     var fano = document.getElementById('txtano');
     var res = document.querySelector('div#res');
-    if(fano.value.length == 0 || fano.value > ano) {
+    if(fano.value.length == 0 || fano.value > ano) { //teste caso usuário insira nada ou um valor de ano maior que o ano atual
         alert('[ERRO] Verifique os dados e tente novamente!');
     } else {
-        var fsex = document.getElementsByName('radsex');
-        var idade = ano -Number(fano.value);
+        var fsex = document.getElementsByName('radsex'); //coleta do dado de sexo do usuários
+        var idade = ano - Number(fano.value); //cálculo da idade. Uso do método "Number()" para que cálculo seja possível, ja que o método ".value" retorna em string.
         var gen = '';
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-        if(fsex[0].checked) {
+        if(fsex[0].checked) { //teste para sexo maculino
             gen= 'homem';
-            if( idade>=0 && idade < 10 ) {
+            if( idade>=0 && idade < 10 ) { //teste para idade
                 img.setAttribute('src', 'bebe-m.png')
             } else if (idade < 21){
                 img.setAttribute('src', 'jovem-m.png')
